44b15155c92fa0a8930ab75d9b4ae53547f5ee42
Hải Long <hailong2004ptcnn@gmail.com>
2025-07-04 22:19:58 +0700

    add omp

--- home.nix
+++ home.nix
@@ -7,7 +7,10 @@
   home.homeDirectory = "/home/veryloooong";
 
   home.packages = with pkgs; [
+    # development
     gh
+    fzf
+    zoxide
   ];
 
   # === DEVELOPMENT ===
@@ -25,15 +28,20 @@
     package = pkgs.vscode.fhs;
   };
 
-  # Shell aliases
-  home.shellAliases = {
-    ls = "ls -l"
+  # === CUSTOMISATION ===
+
+  # oh-my-posh config
+  programs.oh-my-posh = {
+    enable = true;
   };
 
-  # Shell config
   home.file = {
+    # PowerShell config
     ".config/powershell/Microsoft.PowerShell_profile.ps1" = {
-      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix/powershell/profile.ps1"
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix/powershell/profile.ps1";
+    };
+    "./config/omp.toml" = {
+      source = config.lib.file.mkOutOfStoreSymlink "${config.home.homeDirectory}/nix/powershell/omp.toml";
     };
   };
 
