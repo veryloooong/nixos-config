1e027da0c618f31164923500d20ecd6d87d80c64
Hải Long <hailong2004ptcnn@gmail.com>
2025-07-06 21:52:04 +0700

    use auto-cpufreq instead of power-profiles-daemon

--- configuration-laptop.nix
+++ configuration-laptop.nix
@@ -29,6 +29,27 @@
       ];
     })
   ];
+	
+  # Battery and power management
+  services.power-profiles-daemon.enable = false;
+  services.auto-cpufreq = {
+    enable = true;
+    settings = {
+      charger = {
+        governor = "performance";
+        energy_performance_preference = "performance";
+	platform_profile = "performance";
+	turbo = "auto";
+      };
+      battery = {
+        governor = "powersave";
+        energy_performance_preference = "balance_power";
+	platform_profile = "low-power";
+	scaling_max_freq = 3200000;
+	turbo = "never";
+      };
+    };
+  };
 
   # Gaming
   programs.gamescope.enable = true;
