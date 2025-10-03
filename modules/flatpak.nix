ec128410d80c1f3f6f0836f8ad6f95a4b8760363
Hải Long <hailong2004ptcnn@gmail.com>
2025-10-03 21:52:56 +0700

    update flatpaks add stremio 5

--- modules/flatpak.nix
+++ modules/flatpak.nix
@@ -2,6 +2,17 @@
 { ... }:
 
 {
+  services.flatpak.remotes = [
+    {
+      name = "flathub";
+      location = "https://dl.flathub.org/repo/flathub.flatpakrepo";
+    }
+    {
+      name = "flathub-beta";
+      location = "https://flathub.org/beta-repo/flathub-beta.flatpakrepo";
+    }
+  ];
+
   services.flatpak.uninstallUnmanaged = true;
 
   services.flatpak.packages = [
@@ -9,5 +20,6 @@
     "app.twintaillauncher.ttl"
     "org.vinegarhq.Sober"
     "com.bishwasaha.Koncentro"
+    { appId = "com.stremio.Stremio"; origin = "flathub-beta"; }
   ];
 }
